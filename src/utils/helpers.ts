5f1611ab2a33a5ba02b6cee24cd4729ae34cffc1
Alexey Obryadin <obryadin.alexey@gmail.com>
2023-11-04 14:51:40 +0300

    modify helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,10 +1,25 @@
-export const sortArrayByField = <T>(arr: T[], column: keyof T) =>
-	arr.sort((a, b) => {
-		if (a[column] < b[column]) {
-			return -1;
+import { Order } from '../components/app/types';
+
+export const sortArrayByField = <T>(arr: T[], order: Order<T>): T[] => {
+	const { column, direction } = order;
+
+	return arr.sort((a, b) => {
+		if (direction === 'asc') {
+			if (a[column] < b[column]) {
+				return -1;
+			}
+			if (a[column] > b[column]) {
+				return 1;
+			}
+			return 0;
+		} else {
+			if (a[column] > b[column]) {
+				return -1;
+			}
+			if (a[column] < b[column]) {
+				return 1;
+			}
+			return 0;
 		}
-		if (a[column] > b[column]) {
-			return 1;
-		}
-		return 0;
 	});
+};
